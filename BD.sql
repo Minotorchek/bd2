16d4ce7c84e49b45a28f62603cce643199924935
Stepan Rassolov <stepa.rassolov@mail.ru>
2023-01-05 14:27:56 +0500

    fix2

--- BD.sql
+++ BD.sql
@@ -17,32 +17,50 @@ create table if not EXISTS interval1 (
 
 create table if not exists alboms (
 	id SERIAL primary key,
-	albom_id INTEGER not null references performers(id),
+	performers_id INTEGER not null references performers(id),
+	name VARCHAR(60) not null,
 	date1 DATE not null
 );
 
+drop table compilations;
+
+drop table interval3;
+
 create table if not EXISTS interval2 (
 	performers_id INTEGER REFERENCES performers(id),
-	albom_id INTEGER REFERENCES alboms(id),
-	CONSTRAINT x PRIMARY KEY (performers_id, albom_id)
+	performer_id INTEGER REFERENCES alboms(id),
+	CONSTRAINT x PRIMARY KEY (performers_id, performer_id)
 );
 
 CREATE TABLE IF NOT EXISTS music (
 	id SERIAL PRIMARY KEY,
 	music VARCHAR(60) NOT NULL,
-	duration TIME NOT NULL
+	albom_id INTEGER not null references alboms(id),
+	duration time NOT NULL
 );
 	
 create table if not exists compilations (
 	id SERIAL PRIMARY KEY,
-	compilation VARCHAR(60) not null,
+	CompilationName VARCHAR(60) not null,
+	music INTEGER not null references music(id),
+	albom INTEGER not null references alboms(id),
 	date1 DATE not null
 );
 
 CREATE TABLE IF NOT EXISTS interval3 (
 	music_id INTEGER REFERENCES music(id),
 	compilations_id INTEGER REFERENCES compilations(id),
-	CONSTRAINT z PRIMARY KEY (music_id, compilations_id)
+	albom_id INTEGER references alboms(id),
+	CONSTRAINT z PRIMARY KEY (music_id, compilations_id,albom_id)
 );
+
+
+
+
+
+
+
+
+
 
 
---
+++ insert.sql
@@ -0,0 +1,191 @@
+insert into genre(id,genre)
+values (1,'Хип-хоп');
+
+insert into genre(id,genre)
+values (2,'Панк');
+
+insert into genre(id,genre)
+values (3,'Поп');
+
+insert into genre(id,genre)
+values (4,'k-pop');
+
+insert into genre(id,genre)
+values (5,'Диско');
+
+insert into performers
+values (1,4,'BTS');
+
+insert into performers
+values (2,4,'TWICE');
+
+insert into performers 
+values (3,4,'BLACKPINK');
+
+insert into performers
+values (4,1,'Эльдар Джарахов');
+
+insert into performers
+values (5,1,'XXXTentacion');
+
+insert into performers 
+values (6,2,'Сектор газа');
+
+insert into performers 
+values (7,2,'Король и Шут');
+
+insert into performers 
+values (8,3,'Дима Билан');
+
+insert into performers 
+values (9,3,'Егор Крид');
+
+insert into alboms
+values (1,4,'ROCKnROFL','18.09.2018');
+
+insert into alboms
+values (2,1,'Be','2020.11.20');
+
+insert into alboms
+values (3,2,'Celebrate','2022.07.27');
+
+insert into alboms
+values (4,3,'Born Pink','2022.09.16');
+
+insert into alboms
+values (5,5,'?','2017.08.24');
+
+insert into alboms
+values (6,6,'Сектор газа','1995.04.26');
+
+insert into alboms
+values (7,7,'Продавец кошмаров','2006.12.05');
+
+insert into alboms
+values (8,8,'Перегрузка','2020.04.27');
+
+insert into music 
+values (1,'Весна',8,'5:43');
+
+insert into music
+values (2,'Химия',8,'3:20');
+
+insert into music
+values(3,'stay',2,'3:25');
+
+insert into music
+values (4,'skit',2,'3:20');
+
+insert into music
+values (5,'Telepathy',2,'3:22');
+
+insert into music
+values (6,'ПЬЁМ', 1,'3:40');
+
+insert into music
+values (7,'ПОКЕБОЛ',1,'2:21');
+
+insert into music
+values (8,'ДЕЛОРИАН',1,'3:22');
+
+insert into music
+values (9,'Celebrate',3,'3:09');
+
+insert into music
+values (10,'Doughnut',3,'4:24');
+
+insert into music
+values (11,'Typa Girl',4,'3:00');
+
+insert into music
+values (12,'Shut Down',4,'2:56');
+
+insert into music
+values (13,'NUMB',5,'3:06');
+
+insert into music
+values (14,'SAD',5,'2:47');
+
+insert into music
+values (15,'Сектор газа',6,'3:39');
+
+insert into compilations
+values (1,'camp1',3,2,'2022-01-03');
+
+insert into compilations
+values (2,'camp1',7,1,'2022-01-03');
+
+insert into compilations
+values (3,'camp1',5,2,'2022-01-03');
+
+insert into compilations
+values (4,'camp2',2,8,'2022-05-24');
+
+insert into compilations
+values (5,'camp2',1,8,'2022-01-03');
+
+insert into compilations
+values (6,'camp2',3,2,'2022-01-03');
+
+insert into compilations
+values (7,'camp2',15,6,'2022-01-03');
+
+insert into compilations
+values (8,'camp3',4,2,'2021-12-13');
+
+insert into compilations
+values (9,'camp3',13,5,'2021-12-13');
+
+insert into compilations
+values (10,'camp3',10,3,'2021-12-13');
+
+insert into compilations
+values (11,'camp4',9,3,'2021-12-13');
+
+insert into compilations
+values (12,'camp4',6,1,'2021-12-13');
+
+insert into compilations
+values (13,'camp5',8,3,'2020-08-15');
+
+insert into compilations
+values (14,'camp5',11,3,'2020-08-15');
+
+insert into compilations
+values (15,'camp5',13,5,'2020-08-15');
+
+insert into compilations
+values (16,'camp5',4,2,'2020-08-15');
+
+insert into compilations
+values (17,'camp6',8,3,'2020-06-01');
+
+insert into compilations
+values (18,'camp6',12,4,'2020-06-01');
+
+insert into compilations
+values (19,'camp6',7,1,'2020-06-01');
+
+insert into compilations
+values (20,'camp6',2,8,'2020-06-01');
+
+insert into compilations
+values (21,'camp7',10,3,'2020-05-16');
+
+insert into compilations
+values (22,'camp7',4,2,'2020-05-16');
+
+insert into compilations
+values (23,'camp7',14,5,'2020-05-16');
+
+insert into compilations
+values (24,'camp7',9,3,'2020-05-16');
+
+insert into compilations
+values (25,'camp8',6,1,'2022-12-28');
+
+insert into compilations
+values (26,'camp8',13,5,'2022-12-28');
+
+insert into compilations
+values (27,'camp8',3,2,'2022-12-28');
